0c30dbe8cd90ef4faa9e87eafdaf2613bc85ae06
Luiz Pavanello <luizpavanello@gmail.com>
2021-08-23 16:31:06 -0300

    First Commit

---
+++ basics.py
@@ -0,0 +1,10 @@
+print('Hello Simplilear')
+print('concatena' + 'tion')
+# concatenation
+print('concatena', 'tion')
+# concatena tion
+print('I\'m')
+# I'm
+print('He said "Hi!"')
+
+# print('It\'s $' 30) => SyntaxError: invalid syntax
